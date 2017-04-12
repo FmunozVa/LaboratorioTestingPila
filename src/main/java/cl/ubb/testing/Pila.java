8518f0215aa4dd54433155980233c4bbcd5d4e9b
Felipe <fmunozv@alumnos.ubiobio.cl>
2017-04-12 12:05:49 -0300

    primer test, pila vacia

--- src/main/java/cl/ubb/testing/Pila.java
+++ src/main/java/cl/ubb/testing/Pila.java
@@ -1,5 +1,12 @@
 package cl.ubb.testing;
-
 public class Pila {
-
+	private Pila pila [];
+public Pila(){
+	pila=new Pila[0];
+}
+public int Tamaño(){
+	int cant;
+	cant=pila.length;
+	return cant;
+}
 }
--- src/test/java/cl/ubb/testing/TestPila.java
+++ src/test/java/cl/ubb/testing/TestPila.java
@@ -5,11 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestPila {
-
+	Pila pila=new Pila();
 	@Test
 	public void IsEmpty() {
-		
-		
+		int result=pila.Tamaño();
+		assertEquals(0,result);
+				
 	}
 
 }
