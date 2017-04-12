b3504da3a234ce9559d2826faff75351eb1402b4
Felipe <fmunozv@alumnos.ubiobio.cl>
2017-04-12 12:22:07 -0300

    segundo test, pila no vacia, correccion

--- src/main/java/cl/ubb/testing/Pila.java
+++ src/main/java/cl/ubb/testing/Pila.java
@@ -3,7 +3,7 @@ public class Pila {
 	private int [] pila;
 	private int tope;
 	public Pila(){
-	pila=new int[1];
+	pila=new int[5];
 	tope=0;
 }
 public int Tamaño(){
--- src/test/java/cl/ubb/testing/TestPila.java
+++ src/test/java/cl/ubb/testing/TestPila.java
@@ -7,18 +7,20 @@ import org.junit.Test;
 public class TestPila {
 
 	@Test
-	public void IsEmpty() {
+	public void StackIsEmpty() {
 		Pila pila=new Pila();
 		int result=pila.Tamaño();
 		assertEquals(0,result);
 				
 	}
 	@Test
-	public void IsNotEmpty() {
+	public void AddThreeStackIsNotEmpty() {
 		Pila pila=new Pila();
 		pila.add(3);
 		int result= pila.Tamaño();
 		assertEquals(1,result);
 				
 	}
+	
+	
 }
